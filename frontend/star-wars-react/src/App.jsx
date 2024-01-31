d6240ec00784d079138dc51523508e3100ddfc60
craig <fake@email.com>
2024-01-31 14:35:19 -0500

    add film page

--- frontend/star-wars-react/src/App.jsx
+++ frontend/star-wars-react/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, Navigate, Link, NavLink } from 'react-router-dom'
 import './App.css'
 import { Home } from './components/Home'
 import { Character } from './components/Character'
+import { Film } from './components/Film'
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/character/:id" element={<Character />} />
+        <Route path="/film/:id" element={<Film />} />
       </Routes>
     </>
   )
